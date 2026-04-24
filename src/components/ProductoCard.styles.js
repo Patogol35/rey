b57330aca8a3409ea9712cc1ba419fa0f350f784
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-24 18:22:15 -0500

    Update ProductoCard.styles.js

--- src/components/ProductoCard.styles.js
+++ src/components/ProductoCard.styles.js
@@ -120,11 +120,12 @@ export const precioStackSx = (theme) => ({
 // ================================
 // DIVIDER
 // ================================
-export const dividerSx = (theme) => ({
-  my: 1.2,
-  borderColor: theme.palette.divider,
-  transition: "none", // 🔥 clave
-});
+export const dividerSx = {
+  my: 1.8,
+  height: "1px",
+  border: "none",
+  background: "linear-gradient(to right, transparent, rgba(0,0,0,0.2), transparent)",
+};
 
 
 // ================================
