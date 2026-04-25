61d036d249d6bc4a82a9d59b4676c5a32c11fdd2
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-24 20:24:48 -0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -182,7 +182,7 @@ export default function Login() {
 
   return (
     <Container maxWidth="xs" sx={loginStyles.container(theme)}>
-      <Paper elevation={8} sx={loginStyles.paper(theme)}>
+  <Paper elevation={8} sx={loginStyles.paper(theme)}>
         <Typography
           variant="h4"
           align="center"
