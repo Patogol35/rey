234a63571a301baa46015fa8abed5896b7f65427
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-25 10:55:10 -0500

    Update Register.styles.js

--- src/pages/Register.styles.js
+++ src/pages/Register.styles.js
@@ -1,3 +1,5 @@
+import { alpha } from "@mui/material/styles";
+
 const registerStyles = {
   // CONTENEDOR
   container: () => ({
@@ -19,11 +21,10 @@ const registerStyles = {
 
     
     border: "1px solid",
-
-    borderColor:
+borderColor:
   theme.palette.mode === "dark"
-    ? "rgba(255,255,255,0.5)" // más blanco visible
-    : "rgba(0,0,0,0.4)",      // más negro visible
+    ? alpha("#fff", 0.45)
+    : alpha("#000", 0.45),
 
     boxShadow: "0 12px 24px rgba(0,0,0,0.25)",
 
