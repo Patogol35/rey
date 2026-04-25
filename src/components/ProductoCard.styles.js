744009b222559ef21809db51511ea0e8cf1c01d0
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-24 19:12:10 -0500

    Update ProductoCard.styles.js

--- src/components/ProductoCard.styles.js
+++ src/components/ProductoCard.styles.js
@@ -10,8 +10,8 @@ export const cardSx = (theme) => ({
   border: "1px solid",
   borderColor:
   theme.palette.mode === "dark"
-    ? "rgba(255,255,255,0.25)" // blanco suave
-    : "rgba(0,0,0,0.2)",       // negro suave
+    ? "rgba(255,255,255,0.5)" // más blanco visible
+    : "rgba(0,0,0,0.4)",      // más negro visible
 
   boxShadow:
     theme.palette.mode === "dark"
